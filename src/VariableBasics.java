eeb43c713455d9e5d3f73516626f0c56f4bf0046
Cezary <cezaryr54@gmail.com>
2026-01-28 19:39:32 +0100

    oprettet rep

--- src/VariableBasics.java
+++ src/VariableBasics.java
@@ -74,10 +74,114 @@ sb1.append("! ");
 sb1.append("You are ");
 sb1.append(age);
 sb1.append(" years old.");
-    System.out.print(sb1.toString());
+    System.out.print(sb1);
+    System.out.println(empty);
 
     //Opgave 6
     System.out.println("Opgave 6");
+int score=(100);
+    System.out.println(score);
+score +=50;
+    System.out.println(score);
+score +=25;
+    System.out.println(score);
+
+        System.out.println(empty);
+
+    //Opgave 7
+    System.out.println("Opgave 7");
+
+int health=(100);
+    System.out.println(health);
+health -=15;
+    System.out.println(health);
+health -=20;
+    System.out.println(health);
+
+        System.out.println(empty);
+
+    //Opgave 8
+    System.out.println("Opgave 8");
+
+
+double price=(50.0);
+    System.out.println(price);
+price*=2;
+    System.out.println(price);
+
+        System.out.println(empty);
+
+    //Opgave 9
+    System.out.println("Opgave 9");
+
+int counter=(10);
+    System.out.println(counter);
+counter ++;
+    System.out.println(counter);
+counter --;
+    System.out.println(counter);
+
+    System.out.println(empty);
+
+    //Opgave 10
+    System.out.println("Opgave 10");
+
+
+int number=(10);
+number +=5;
+    System.out.println(number);
+number -=3;
+    System.out.println(number);
+number ++;
+    System.out.println(number);
+number --;
+    System.out.println(number);
+
+//Opgave 11
+String productName=("Laptop");
+double itemCost=(7999.99);
+boolean inStock=(true);
+int numberInStock=(15);
+double itemDiscount=(10.0);
+String productCategory=("Electronics");
+
+//Opgave 12
+String studentName=("Anna");
+int testPoints=(85);
+int assignmentPoints=(92);
+double avgGrade=((testPoints+assignmentPoints)/2);
+boolean coursePassed=(true);
+
+//Opgave 13
+
+String guestName=("Peter Hansen");
+int guestRoom=(204);
+int guestStayingNights=(3);
+double nightPrice=(899.00);
+boolean breakfastIncluded=(true);
+double guestTotalPrice=(2697.00);
+
+//Opgave 14
+
+int stepsToday=(8547);
+int stepGoal=(10000);
+double kmWalked=(6.2);
+int caloriesBurned=(420);
+boolean reachedGoal=(false);
+
+//Opgave 15
+
+String bookTitle;
+String authorsName;
+String bookISBN;
+int pageCount;
+boolean isAvailable; //if false > borrowed by. Else. True.
+String borrowedBy;
+String returnDate;
+int lateFee;
+boolean bestseller;
+double bookRating;
+
 
 
 
